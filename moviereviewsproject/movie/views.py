839ce2edbd8c2130a49cfb7ec5a8c3ec80b5bd11
melisaruaH <149987428+melisaruaH@users.noreply.github.com>
2023-11-06 20:42:59 -0500

    views.py

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 def home(request):
   #return HttpResponse('<h1>welcome to home page</h1>')
   #return render(request, 'home.html')
-  return render(request, 'home.html', {'name':'Greg Lim'})
+  return render(request, 'home.html', {'name':'Melisa Rua'})
 
